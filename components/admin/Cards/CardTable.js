a4d2a5506c5f6bc2ac45e748c655a5449bdf403c
denyFh <72296448+denyFh@users.noreply.github.com>
2022-06-13 18:21:54 +0700

    add member modal

--- components/admin/Cards/CardTable.js
+++ components/admin/Cards/CardTable.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import PropTypes from "prop-types";
 
-import { PencilIcon, TrashIcon } from "@heroicons/react/solid";
+import { Dialog, Transition, Listbox } from "@headlessui/react";
+import { PencilIcon, TrashIcon, SelectorIcon, CheckIcon } from "@heroicons/react/solid";
 
 // components
-
 import TableDropdown from "../Dropdowns/TableDropdown.js";
 
+const specialist = [
+  { name: 'Frontend Engineer' },
+  { name: 'Mobile Engineer' },
+  { name: 'UI/UX Engineer' },
+  { name: 'Backend Engineer' },
+  { name: 'Quality Engineer' },
+]
+
 export default function CardTable({ color, title, sidebutton }) {
+
+  let [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(specialist[0])
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
   return (
     <>
       <div
@@ -30,7 +50,10 @@ export default function CardTable({ color, title, sidebutton }) {
             </div>
             {
               sidebutton ? (
-                <button className="px-3 py-2 mx-2 text-xs font-medium text-white rounded bg-darkGreen">
+                <button
+                  className="px-3 py-2 mx-2 text-xs font-medium text-white rounded bg-darkGreen"
+                  type="button"
+                  onClick={openModal}>
                   + ADD MEMBER COURSE
                 </button>
               ) : ("")
@@ -346,6 +369,177 @@ export default function CardTable({ color, title, sidebutton }) {
             </tbody>
           </table>
         </div>
+        <Transition appear show={isOpen} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={closeModal}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-opacity-75 bg-slate-800" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex items-center justify-center min-h-full p-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <Dialog.Panel className="w-full max-w-md p-6 text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                    <Dialog.Title
+                      as="h3"
+                      className="mb-6 text-lg font-medium leading-6 text-gray-900"
+                    >
+                      Add Member Course
+                    </Dialog.Title>
+
+                    <form action="">
+                      <div className="grid grid-cols-2 gap-4">
+                        <div className="mb-6 form-group">
+                          <label htmlFor="">First Name</label>
+                          <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput123" aria-describedby="emailHelp123" placeholder="First name" />
+                        </div>
+                        <div className="mb-6 form-group">
+                          <label htmlFor="">Last Name</label>
+                          <input type="text" className="form-control
+                              block
+                              w-full
+                              px-3
+                              py-1.5
+                              text-base
+                              font-normal
+                              text-gray-700
+                              bg-white bg-clip-padding
+                              border border-solid border-gray-300
+                              rounded
+                              transition
+                              ease-in-out
+                              m-0
+                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput124"
+                            aria-describedby="emailHelp124" placeholder="Last name" />
+                        </div>
+                      </div>
+                      <div className="mb-6 form-group">
+                        <label htmlFor="">Email Password</label>
+                        <input type="email" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput125" placeholder="Email address" />
+                      </div>
+                      <div className="mb-6 form-group">
+                        <label htmlFor="">Password</label>
+                        <input type="password" className="form-control block
+                              w-full
+                              px-3
+                              py-1.5
+                              text-base
+                              font-normal
+                              text-gray-700
+                              bg-white bg-clip-padding
+                              border border-solid border-gray-300
+                              rounded
+                              transition
+                              ease-in-out
+                              m-0
+                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput126"
+                          placeholder="Password" />
+                      </div>
+                      <div className="mb-6 form-group">
+                        <label htmlFor="">Confirm Password</label>
+                        <input type="password" className="form-control block
+                              w-full
+                              px-3
+                              py-1.5
+                              text-base
+                              font-normal
+                              text-gray-700
+                              bg-white bg-clip-padding
+                              border border-solid border-gray-300
+                              rounded
+                              transition
+                              ease-in-out
+                              m-0
+                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput126"
+                          placeholder="Password" />
+                      </div>
+                      <Listbox value={selected} onChange={setSelected}>
+                        <label htmlFor="">Specialist</label>
+                        <div className="relative mt-1">
+                          <Listbox.Button className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
+                            <span className="block truncate">{selected.name}</span>
+                            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                              <SelectorIcon
+                                className="w-5 h-5 text-gray-400"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          </Listbox.Button>
+                          <Transition
+                            as={Fragment}
+                            leave="transition ease-in duration-100"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                          >
+                            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                              {specialist.map((specialist, specialistIdx) => (
+                                <Listbox.Option
+                                  key={specialistIdx}
+                                  className={({ active }) =>
+                                    `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                                    }`
+                                  }
+                                  value={specialist}
+                                >
+                                  {({ selected }) => (
+                                    <>
+                                      <span
+                                        className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                                          }`}
+                                      >
+                                        {specialist.name}
+                                      </span>
+                                      {selected ? (
+                                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                          <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                                        </span>
+                                      ) : null}
+                                    </>
+                                  )}
+                                </Listbox.Option>
+                              ))}
+                            </Listbox.Options>
+                          </Transition>
+                        </div>
+                      </Listbox>
+                      <div className="flex flex-row-reverse w-full gap-3 mt-4">
+                        <button
+                          type="button"
+                          className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md bg-darkGreen focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                          onClick={closeModal}
+                        >
+                          Add Member
+                        </button>
+                        <button
+                          type="button"
+                          className="inline-flex justify-center px-4 py-2 text-sm font-medium text-black border border-transparent border-green-800 rounded-md hover:bg-green-800 hover:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                          onClick={closeModal}
+                        >
+                          Close
+                        </button>
+                      </div>
+                    </form>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition>
       </div>
     </>
   );
