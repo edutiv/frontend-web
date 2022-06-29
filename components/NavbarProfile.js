6eee578adb288b5bbc6945b884d1b3c5c3545cdd
denyFh <72296448+denyFh@users.noreply.github.com>
2022-06-29 21:30:15 +0700

    fixing minor bug for navbar and image

--- components/NavbarProfile.js
+++ components/NavbarProfile.js
@@ -217,34 +217,212 @@ export default function NavbarProfile() {
         </div>
       </div>
 
-        <div className="grid content-center ">
-        <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 pl-3 pr-4 py-3 md:hover:text-blue-700 md:p-0 font-medium flex items-center justify-between w-full md:w-auto">
-            Hi, Annete <Image
-                            src={mentor}
-                            alt="user photo"
-                            className=" rounded-full w-12 "
-                            /> <svg className="w-4 h-4"fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
-                {/* <!-- Dropdown menu --> */}
-                <div id="dropdownNavbar" class="hidden bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow my-4 w-44">
-                    <ul class="py-1" aria-labelledby="dropdownLargeButton">
-                        <li>
-                            <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Edit Profile</a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">My Course</a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Certificate</a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Progress Course</a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Logout</a>
-                        </li>
-                    </ul>
-        </div>
+      <div className="grid content-center ">
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <Menu.Button className="inline-flex justify-center items-center gap-2 w-full bg-white font-medium px-2 py-[0.5rem] rounded text-gray-700 hover:bg-gray-50">
+              <p>
+                Hi, Annette
+              </p>
+              <Image
+                src={mentor}
+                alt="user photo"
+                className="rounded-full max-h-8"
+                width={40}
+                height={40}
+              />
+              <ChevronDownIcon
+                className="w-4 h-6 text-sm"
+                aria-hidden="true"
+              />
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 py-2 mt-2 origin-top-right bg-white rounded-md shadow-lg w-80 ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="mx-4 ">
+                <Menu.Item>
+                  {({ active }) => (
+                    <MyLink href="/test">
+                      <div className="grid grid-cols-5 px-2 py-2">
+                        <div className="w-10 h-10 p-0 rounded-full">
+                          <Image
+                            src={allCourse}
+                            alt="Course1"
+                            className="object-scale-down rounded-full"
+                            layout="responsive"
+                            loading="lazy"
+                          />
+                        </div>
+                        <div className="col-span-4 ">
+                          <h1
+                            className={classNames(
+                              active ? " text-gray-400" : "text-gray-600/100",
+                              " text-base font-semibold"
+                            )}
+                          >
+                            All Course
+                          </h1>
+                          <p className="text-xs text-slate-300 "> Lots of interesting courses ready to learn </p>
+                        </div>
+                      </div>
+                    </MyLink>
+                  )}
+                </Menu.Item>
+
+                <Menu.Item>
+                  {({ active }) => (
+                    <MyLink href="#">
+                      <div className="grid grid-cols-5 px-2 py-2">
+                        <div className="w-10 h-10 p-0 rounded-full">
+                          <Image
+                            src={backendCourse}
+                            alt="Course1"
+                            className="object-scale-down rounded-full"
+                            layout="responsive"
+                            loading="lazy"
+                          />
+                        </div>
+                        <div className="col-span-4 ">
+                          <h1
+                            className={classNames(
+                              active ? " text-gray-400" : "text-gray-600/100",
+                              " text-base font-semibold"
+                            )}
+                          >
+                            Backend Engineer
+                          </h1>
+                          <p className="text-xs text-slate-300 "> Full-Stack Web & Mobile Developer </p>
+                        </div>
+                      </div>
+                    </MyLink>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <MyLink href="/test">
+                      <div className="grid grid-cols-5 px-2 py-2">
+                        <div className="w-10 h-10 p-0 rounded-full">
+                          <Image
+                            src={frontendCourse}
+                            alt="Course1"
+                            className="object-scale-down rounded-full"
+                            layout="responsive"
+                            loading="lazy"
+                          />
+                        </div>
+                        <div className="col-span-4 ">
+                          <h1
+                            className={classNames(
+                              active ? " text-gray-400" : "text-gray-600/100",
+                              " text-base font-semibold"
+                            )}
+                          >
+                            Frontend Engineer
+                          </h1>
+                          <p className="text-xs text-slate-300 "> UI/UX & Graphic Design </p>
+                        </div>
+                      </div>
+                    </MyLink>
+                  )}
+                </Menu.Item>
+
+                <Menu.Item>
+                  {({ active }) => (
+                    <MyLink href="#">
+                      <div className="grid grid-cols-5 px-2 py-2">
+                        <div className="w-10 h-10 p-0 rounded-full">
+                          <Image
+                            src={mobileCourse}
+                            alt="Course1"
+                            className="object-scale-down rounded-full"
+                            layout="responsive"
+                            loading="lazy"
+                          />
+                        </div>
+                        <div className="col-span-4 ">
+                          <h1
+                            className={classNames(
+                              active ? " text-gray-400" : "text-gray-600/100",
+                              " text-base font-semibold"
+                            )}
+                          >
+                            Mobile Engineer
+                          </h1>
+                          <p className="text-xs text-slate-300 "> Improve your skills </p>
+                        </div>
+                      </div>
+                    </MyLink>
+                  )}
+                </Menu.Item>
+
+                <Menu.Item>
+                  {({ active }) => (
+                    <MyLink href="#">
+                      <div className="grid grid-cols-5 px-2 py-2">
+                        <div className="w-10 h-10 p-0 rounded-full">
+                          <Image
+                            src={uiuxCourse}
+                            alt="Course1"
+                            className="object-scale-down rounded-full"
+                            layout="responsive"
+                            loading="lazy"
+                          />
+                        </div>
+                        <div className="col-span-4 ">
+                          <h1
+                            className={classNames(
+                              active ? " text-gray-400" : "text-gray-600/100",
+                              " text-base font-semibold"
+                            )}
+                          >
+                            UI/UX Designer
+                          </h1>
+                          <p className="text-xs text-slate-300 "> Improve your skills </p>
+                        </div>
+                      </div>
+                    </MyLink>
+                  )}
+                </Menu.Item>
+
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+        {/* <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 pl-3 pr-4 py-3 md:hover:text-blue-700 md:p-0 font-medium flex items-center justify-between w-full md:w-auto">
+          Hi, Annete <Image
+            src={mentor}
+            alt="user photo"
+            className=" rounded-full w-12 "
+          /> <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></button> */}
+        {/* <!-- Dropdown menu --> */}
+        {/* <div id="dropdownNavbar" className="hidden bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow my-4 w-44">
+          <ul className="py-1" aria-labelledby="dropdownLargeButton">
+            <li>
+              <a href="#" className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Edit Profile</a>
+            </li>
+            <li>
+              <a href="#" className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">My Course</a>
+            </li>
+            <li>
+              <a href="#" className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Certificate</a>
+            </li>
+            <li>
+              <a href="#" className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Progress Course</a>
+            </li>
+            <li>
+              <a href="#" className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Logout</a>
+            </li>
+          </ul>
+        </div> */}
+      </div>
     </div>
-</div>
-    );
+  );
 }
--- pages/profileCertificate.js
+++ pages/profileCertificate.js
@@ -4,42 +4,41 @@ import Image from "next/image";
 import NavbarProfile from "../components/NavbarProfile";
 import CardCertificate from "../components/CardCertificate";
 
-import { DownloadIcon } from "@heroicons/react/solid";
 import CertificateImg from '../public/assets/img/Certificate.png'
 
 function profileCertificate() {
     return (
         <div>
             <Head>
-				<title>My Course</title>
-				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-			</Head>
+                <title>My Course</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
             <header>
                 <NavbarProfile />
             </header>
-			<main>
-				<div className="pl-20 pr-20 grid content-center md:mt-16 ">
-					<div className="grid content-center grid-cols-3 gap-8 md:grid-cols-4">
-						<CardProfile />
-						<div className="col-span-3 px-4 py-4 border-[1px] border-[#C2C2C2] rounded-md justify-center align-center items-center">
-							<div className="mt-2 content-center">
-								<p>Certificate</p>
-							</div>
-							<p className=" text-xs mt-1 text-[#C2C2C2]">
-								Keep upgrading your knowledge and experience your latest
-								technology
-							</p>
-                            <div className="grid grid-cols-3 gap-5 my-6">
-                                <CardCertificate image={CertificateImg} logo={DownloadIcon} course={"Mastering Front-End Development with React JS"} finish={"Finish 12 January 2022"} ></CardCertificate>
-                                <CardCertificate image={CertificateImg} logo={DownloadIcon} course={"Mastering Mobile Development with Flutter"} finish={"Finish 12 January 2022"} ></CardCertificate>
-                                <CardCertificate image={CertificateImg} logo={DownloadIcon} course={"Introduction to Backend Enginner with Golang"} finish={"Finish 12 January 2022"} ></CardCertificate>
-                                <CardCertificate image={CertificateImg} logo={DownloadIcon} course={"Mastering Front-End Development with React JS"} finish={"Finish 12 January 2022"} ></CardCertificate>
-                                <CardCertificate image={CertificateImg} logo={DownloadIcon} course={"Mastering Mobile Development with Flutter"} finish={"Finish 12 January 2022"} ></CardCertificate>
-                                <CardCertificate image={CertificateImg} logo={DownloadIcon} course={"Introduction to Backend Enginner with Golang"} finish={"Finish 12 January 2022"} ></CardCertificate>
+            <main>
+                <div className="pl-20 pr-20 grid content-center md:mt-16 ">
+                    <div className="grid content-center grid-cols-3 gap-8 md:grid-cols-4">
+                        <CardProfile />
+                        <div className="col-span-3 px-4 py-4 border-[1px] border-[#C2C2C2] rounded-md justify-center align-center items-center">
+                            <div className="mt-2 content-center">
+                                <p>Certificate</p>
                             </div>
+                            <p className=" text-xs mt-1 text-[#C2C2C2]">
+                                Keep upgrading your knowledge and experience your latest
+                                technology
+                            </p>
+                            <div className="grid grid-cols-3 gap-5 my-6">
+                                <CardCertificate image={CertificateImg} course={"Mastering Front-End Development with React JS"} finish={"Finish 12 January 2022"} />
+                                {/* <CardCertificate image={CertificateImg} logo={DownloadIcon} course={"Mastering Mobile Development with Flutter"} finish={"Finish 12 January 2022"} ></CardCertificate>
+                                <CardCertificate image={CertificateImg} logo={DownloadIcon} course={"Introduction to Backend Enginner with Golang"} finish={"Finish 12 January 2022"} ></CardCertificate>
+                                <CardCertificate image={CertificateImg} logo={DownloadIcon} course={"Mastering Front-End Development with React JS"} finish={"Finish 12 January 2022"} ></CardCertificate>
+                                <CardCertificate image={CertificateImg} logo={DownloadIcon} course={"Mastering Mobile Development with Flutter"} finish={"Finish 12 January 2022"} ></CardCertificate>
+                                <CardCertificate image={CertificateImg} logo={DownloadIcon} course={"Introduction to Backend Enginner with Golang"} finish={"Finish 12 January 2022"} ></CardCertificate> */}
                             </div>
                         </div>
                     </div>
+                </div>
             </main>
         </div>
     )
