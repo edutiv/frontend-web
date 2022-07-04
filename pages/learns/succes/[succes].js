7c1622503a294beedeafd77e4fa89aa38ffa9dc6
denyFh <72296448+denyFh@users.noreply.github.com>
2022-07-04 12:14:30 +0700

    fixing component name path

--- pages/learns/succes/[succes].js
+++ pages/learns/succes/[succes].js
@@ -6,7 +6,7 @@ import ModalRating from "../../../components/ModalRating";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import ModalCertificate from "../../../components/modalCertificate";
+import ModalCertificate from "../../../components/ModalCertificate";
 import Navbar from "../../../components/Navbar";
 import { BASE_URL } from '../../../config/API'
 
