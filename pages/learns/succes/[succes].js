d672c839fabeedfb94795faa66affd8ea7f9d21f
pryy <priandycoy@gmail.com>
2022-07-15 11:28:49 +0700

    page succes: make display data report

--- pages/learns/succes/[succes].js
+++ pages/learns/succes/[succes].js
@@ -8,12 +8,17 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import ModalCertificate from "../../../components/ModalCertificate";
 import Navbar from "../../../components/Navbar";
-import { BASE_URL } from '../../../config/API'
+import { BASE_URL } from "../../../config/API";
+import Cookies from "universal-cookie";
 
 export default function SuccesCourse() {
   const [dataCourse, setDataCourse] = useState([]);
   const { query } = useRouter();
   const [hidden, setHidden] = useState(false);
+  const cookies = new Cookies();
+  const [dataUser, setDataUser] = useState();
+  const [dataEnrolled, setDataEnrolled] = useState();
+  const [idEnrolled, setIdEnrolled] = useState();
 
   const handleHidden = () => {
     setHidden(true);
@@ -21,9 +26,7 @@ export default function SuccesCourse() {
 
   const getEdutivData = () => {
     let idCourse = query.succes;
-    let endpoints = [
-      `${BASE_URL}/course/${idCourse}`,
-    ];
+    let endpoints = [`${BASE_URL}/course/${idCourse}`];
 
     if (idCourse) {
       Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
@@ -36,11 +39,88 @@ export default function SuccesCourse() {
     console.log(query.succes);
   };
 
+  const getUserEnrolled = () => {
+    let token = cookies.get("token");
+    let idCourse = query.succes;
+
+    if (token) {
+      
+      axios
+        .get(`${BASE_URL}/user`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+          setDataUser(res.data.data);
+         
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    }
+
+    if (idCourse) {
+      axios
+        .get(`${BASE_URL}/enrolled/courses/${idCourse}`)
+        .then((res) => {
+          setDataEnrolled(res.data.data);
+         
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    }
+  };
+
+  const checkUserEnrolled = () => {
+    const userLogin = dataUser?.firstname;
+    let userEnrolled;
+
+    userEnrolled = dataEnrolled?.filter((user) => {
+      return user.user.firstname == userLogin;
+      // Use the toLowerCase() method to make it case-insensitive
+    });
+    
+
+    userEnrolled?.slice(0).map((item) => {
+      return setIdEnrolled(item.id);
+    });
+  };
+
   useEffect(() => {
-    getEdutivData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    getUserEnrolled();
   }, []);
 
+  useEffect(() => {
+    checkUserEnrolled();
+  });
+
+  const getDataReport = () => {
+    let token = cookies.get("token");
+
+    axios(`${BASE_URL}/enrolled/download-report/${idEnrolled}`, {
+      method: "GET",
+      responseType: "blob", //Force to receive data in a Blob Format
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        //Create a Blob from the PDF Stream
+        const file = new Blob([response.data], { type: "application/pdf" });
+        //Build a URL from the file
+        const fileURL = URL.createObjectURL(file);
+        //Open the URL on new Window
+        window.open(fileURL);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getEdutivData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div>
       <Navbar />
@@ -81,6 +161,14 @@ export default function SuccesCourse() {
                   dataCourse={dataCourse}
                   handleHidden={handleHidden}
                 />
+              </div>
+              <div>
+                <button
+                onClick={getDataReport}
+                  className="bg-white md:w-80 w-full py-3 text-[#126E64] rounded-md border-[1px] border-[#E0E0E0] text-[11px] hover:border-[#126E64] hover:-translate-y-[0.15rem] hover:transition hover:duration-100 hover:ease-in-out hover:drop-shadow-md"
+                >
+                  Download Report
+                </button>
               </div>
             </div>
           )}
